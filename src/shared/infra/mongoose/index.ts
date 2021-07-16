a56614033005bba8128d529ce1cb2f77f198a3a3
Levy <levyzudo@msn.com>
2021-07-16 01:47:05 -0300

    fix: fixed exposed test account

--- src/shared/infra/mongoose/index.ts
+++ src/shared/infra/mongoose/index.ts
@@ -1,7 +1,6 @@
 import { connect } from "mongoose";
 
-const uri =
-  "mongodb+srv://carupi:L9JPpmTpFQwtkDOJ@mongo-db-api-cars-carup.mk9is.mongodb.net/db-carupi-cars?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@mongo-db-api-cars-carup.mk9is.mongodb.net/db-carupi-cars?retryWrites=true&w=majority`;
 
 const Connection = () =>
   connect(uri, {
